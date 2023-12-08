912d9db8b3d8d1d961c9570b74ceea16300b07e6
KhoaAnhDoNguyen <117917211+KhoaAnhDoNguyen@users.noreply.github.com>
2023-12-08 08:19:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pickle as pkl
+import numpy as np
+from PIL import Image
+
+ class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
+st.title('Sentiment analysis from Vietnamese student's feedback')
+
+//image = Image.open('ten.png')
+//st.image(image)
 
