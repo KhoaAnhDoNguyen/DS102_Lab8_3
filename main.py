5c3304a527390994a55d74ecaa233d33972ecfc0
KhoaAnhDoNguyen <117917211+KhoaAnhDoNguyen@users.noreply.github.com>
2023-12-18 20:45:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from PIL import Image
 class_list = {'0': 'Negative', '1': 'Neutral', '2': 'Positive'}
 st.title("Sentiment analysis from Vietnamese student's feedback")
 
-#image = Image.open('ten.png')
-#st.image(image)
+image = Image.open('feedback.jpg')
+st.image(image)
 
 input_ec = open('ec_vsfc.pkl', 'rb')
 encoder = pkl.load(input_ec)
